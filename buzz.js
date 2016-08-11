a05b62fa03ad958477a408d493dd88d21b4607f0
Andrew Rich <andrew_rich@hotmail.com>
2016-08-11 13:39:45 -0400

    pseduo code, parseInt function changed function to counter

--- buzz.js
+++ buzz.js
@@ -1,46 +1,41 @@
-// prompt a user for input of a number
-//
-var naumbre = prompt("enter a number");
+// use old code and add a function that meets these requirements
 
-function iceCream(num){
+//page loads, prompt a user for input of a number
+
+//write a nameed function
+	// takes interger as argument
+	//counts from 1 to argument value
+	// substitute fizz, buzz and fizz buzz accordingly
+
+//convert input value from string(default) to number
+	//use + operator for conversion somehow
+
+//validate input with parseInt() function
+	//where to put parseInt()
+
+var naumbre = parseInt(prompt("enter a number"));
+
+
+function counter(num){
 	for (var i = 1; i <= num; i++){
-	if (i % 3 === 0 && i % 5 === 0){
-		console.log("fizzBuzz");
-	}
-	else if(i % 3 === 0){
-		console.log("fizz");
-	}
-	else if(i % 5 === 0){
-		console.log("buzz");
-	}
-	else{
-		console.log(i);
-	}
-	}
-}
-
-iceCream(naumbre);
-// print numbers 1 - 100
-//numbers that are divisible by 3 should print "fizz"
-// number that are divisible by 5 shpuld print "buzz"
-// numbers divisible by 3 & 5 should print "fizz buzz"
-/*
-for (var i = 1; i <= 100; i++){
-	if (i % 3 === 0 && i % 5 === 0){
-		console.log("fizzBuzz");
-	}
-	else if(i % 3 === 0){
-		console.log("fizz");
-	}
-	else if(i % 5 === 0){
-		console.log("buzz");
-	}
-	else{
-		console.log(i);
+		if (i % 3 === 0 && i % 5 === 0){
+			console.log("fizzBuzz");
+		}
+		else if(i % 3 === 0){
+			console.log("fizz");
+		}
+		else if(i % 5 === 0){
+			console.log("buzz");
+		}
+		else{
+			console.log(i);
+		}
 	}
 }
 
 
-*/
+
+counter(naumbre);
+
 
 
