23703f70137e0794cc74a66380ea286691c0a62e
Andrew Rich <andrew_rich@hotmail.com>
2016-08-12 11:49:47 -0400

    added input field and button, droped prompt,appending to dom

--- buzz.js
+++ buzz.js
@@ -1,41 +1,37 @@
-// use old code and add a function that meets these requirements
 
-//page loads, prompt a user for input of a number
-
-//write a nameed function
-	// takes interger as argument
-	//counts from 1 to argument value
-	// substitute fizz, buzz and fizz buzz accordingly
-
-//convert input value from string(default) to number
-	//use + operator for conversion somehow
-
-//validate input with parseInt() function
-	//where to put parseInt()
-
-var naumbre = parseInt(prompt("enter a number"));
+$(document).ready(function(){
 
 
 function counter(num){
 	for (var i = 1; i <= num; i++){
+		
 		if (i % 3 === 0 && i % 5 === 0){
-			console.log("fizzBuzz");
+				$("ul").append('<li>' + "fizzbuzz" +'</li>');
 		}
+
 		else if(i % 3 === 0){
-			console.log("fizz");
+			$("ul").append('<li>' + "fizz" +'</li>');
 		}
+
 		else if(i % 5 === 0){
-			console.log("buzz");
+			$("ul").append('<li>' + "buzz" +'</li>');
 		}
+
 		else{
-			console.log(i);
+			$("ul").append('<li>' + i +'</li>');
 		}
 	}
 }
 
 
+//press a button to run the function called counter, 
+//pass the value/argument from input field into the function
+//not sure if parseInt() is working
+$("#myButton").click(function(){
+	counter(parseInt($("#formValue").val()));
+});
 
-counter(naumbre);
 
 
 
+});
